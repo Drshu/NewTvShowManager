167f94e2899c14a8cd6c2397d0bb282ceba5bbab
Shucheng <741932183@qq.com>
2016-04-05 14:31:37 +0800

    Initial Commit

---
+++ NewTvShowManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewTvShowManager
+//
+//  Created by shucheng on 16/4/5.
+//  Copyright © 2016年 shucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

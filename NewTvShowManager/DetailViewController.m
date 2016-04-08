fdcaa0d2632253cf1e8a0afe1a0c46998031c139
Shucheng <741932183@qq.com>
2016-04-08 20:12:38 +0800

    两视图之间的数据无障碍传递

--- NewTvShowManager/DetailViewController.m
+++ NewTvShowManager/DetailViewController.m
@@ -15,7 +15,7 @@
 @property (weak, nonatomic) IBOutlet UIStepper *yourSeasonStepper;
 @property (weak, nonatomic) IBOutlet UIStepper *lastedSeasonStepper;
 @property (weak, nonatomic) IBOutlet UIStepper *lastedEpisodeStepper;
-@property (weak, nonatomic) IBOutlet UITextField *youeEpisode;
+@property (weak, nonatomic) IBOutlet UITextField *yourEpisode;
 @property (weak, nonatomic) IBOutlet UITextField *yourSeason;
 @property (weak, nonatomic) IBOutlet UITextField *introductionField;
 @property(nonatomic, strong)UILabel * lable;
@@ -34,10 +34,10 @@
         NSString *fileName=[doc stringByAppendingPathComponent:@"tvShow.sqlite"];
         FMDatabase *db=[FMDatabase databaseWithPath:fileName];
     // 1.执行查询语句
-    NSString *findStr = [NSString stringWithFormat:@"SELECT * FROM t_show where name =%@",self.showName];
+    NSString *findStr = [NSString stringWithFormat:@"SELECT * FROM t_show where name = '%@'",self.showName];
         [db open];
     FMResultSet *resultSet = [db executeQuery:findStr];
-
+    //2.提取数据库数据
     if ([db open]) {
     while ([resultSet next]) {
         NSString *name = [resultSet stringForColumn:@"name"];
@@ -46,8 +46,11 @@
         NSLog(@"%@ %@ %@", name, introduction,lastDate);
         self.nameField.text = name;
         self.introductionField.text = introduction;
-        
-         }
+        NSString *yourseasonNumber = [lastDate substringWithRange:NSMakeRange(2, 2)];
+        NSString *yourEpisodeNmeber = [lastDate substringWithRange:NSMakeRange(5, 2)];
+        self.lastedSeason.text = yourseasonNumber;
+        self.lastedEpisode.text = yourEpisodeNmeber;
+    }
     }
 }
 @end
